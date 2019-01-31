60a80e909d8f61770b5eefe8ca84b55ae9a9280e
RedOvO <599236422@qq.com>
2019-01-31 18:03:25 +0800

    finish delete function

--- src/AppList.js
+++ src/AppList.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import './App.css'
 
 class AppList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            List:['aaa', 'bbb'],
+        }
+    }
+    
+    handleClick(index) {
+        console.log(index);
+        let list = this.state.List;
+        list.splice(index, 1);
+        this.setState({
+            List: list,
+        })
+    }
+
     render() {
         return(
             <div className='AppList'>
                 <ul>
-                    <li className='one_input'>
-                        <label>{this.props.value}</label>
-                        <button className='deleteButton'>delete</button>
-                    </li>
+                    {this.state.List.map((item, index) => {
+                        return ( 
+                            <li className='one_input' key={index}>
+                                <label>{item}</label>
+                                <button className='deleteButton' onClick={this.handleClick.bind(this, index)}>delete</button>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
         )
--- src/AppTop.js
+++ src/AppTop.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import './App.css'
 
-class AppTop extends Component{
+class AppTop extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+        }
+        this.handleClick = this.handleClick.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            inputValue: '',
+        })
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            inputValue: e.target.value,
+        })
+    }
+
     render() {
         return (
             <div className='AppTop'>
                 <div className='inputArea'>
-                    <textarea placeholder='input' />
+                    <textarea
+                        placeholder='input'
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange} />
                 </div>
-                <button className='addButton'>Add</button>
+                <button className='addButton' onClick={this.handleClick}>Add</button>
             </div>
         )
     }
